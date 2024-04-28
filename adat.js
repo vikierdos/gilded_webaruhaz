379648843e4df029acba3d3a156942651b9a8dce
vikierdos <csicsorka@gmail.com>
2024-04-28 14:30:33 +0200

    RENAMING

--- adat.js
+++ adat.js
@@ -2,66 +2,310 @@ export const DRESSLIST = [
   {
     name: "Amethyst Glow",
     pic: "photos/amethyst_glow.png",
-    dsc: "A beautiul",
     color: "purple",
-    price: 800
+    price: 690
   },
   {
     name: "Aurora Amour",
     pic: "photos/aurora_amour.png",
-    dsc: "A beautiful",
     color: "white",
-    price: 800
+    price: 520
   },
   {
     name: "Aurora Silk Mirage",
     pic: "photos/aurora_silk_mirage.png",
-    dsc: "A beautiful",
     color: "pink",
-    price: 800
+    price: 690
   },
   {
     name: "Azure Skies Corset",
     pic: "photos/azure_skies_corset.png",
-    dsc: "A beautiful",
     color: "blue",
-    price: 800
+    price: 760
   },
   {
     name: "Barbie Bombshell",
     pic: "photos/barbie_bombshell.png",
-    dsc: "A beautiful",
     color: "pink",
-    price: 800
+    price: 470
   },
   {
     name: "Blossom Breeze",
     pic: "photos/blossom_breeze.png",
-    dsc: "A beautiful",
     color: "purple",
-    price: 800
+    price: 500
   },
   {
-    name: "Bluebell Breeze",
+    name: "Bluebell Bliss",
     pic: "photos/bluebell_bliss.png",
-    dsc: "A beautiful",
     color: "blue",
-    price: 800
+    price: 620
   },
   {
     name: "Celestial Charm",
     pic: "photos/celestial_charm.png",
-    dsc: "A beautiful",
     color: "white",
-    price: 800
+    price: 700
   },
   {
     name: "Cherry Charisma",
     pic: "photos/cherry_charisma.png",
-    dsc: "A beautiful",
     color: "red",
-    price: 800
+    price: 620
+  },
+  {
+    name: "Crimson Velvet Ensemble",
+    pic: "photos/crimson_velvet_ensemble.png",
+    color: "red",
+    price: 699
+  },
+  {
+    name: "Crystal Cascade",
+    pic: "photos/crystal_cascade.png",
+    color: "white",
+    price: 1340
+  },
+  {
+    name: "Crystal Couture",
+    pic: "photos/crystal_couture.png",
+    color: "white",
+    price: 1700
+  },
+  {
+    name: "Dreamscape Symphony",
+    pic: "photos/dreamscape_symphony.png",
+    color: "pink",
+    price: 620
+  },
+  {
+    name: "Emerald Silk Sheath",
+    pic: "photos/emerald_silk_sheath.png",
+    color: "green",
+    price: 560
+  },
+  {
+    name: "Ethereal Prism Poise",
+    pic: "photos/ethereal_prism_poise.png",
+    color: "blue",
+    price: 540
+  },
+  {
+    name: "Cherry Charisma",
+    pic: "photos/cherry_charisma.png",
+    color: "red",
+    price: 620
+  },
+  {
+    name: "Cherry Charisma",
+    pic: "photos/cherry_charisma.png",
+    color: "red",
+    price: 620
+  },
+  {
+    name: "Cherry Charisma",
+    pic: "photos/cherry_charisma.png",
+    color: "red",
+    price: 620
+  },
+  {
+    name: "Cherry Charisma",
+    pic: "photos/cherry_charisma.png",
+    color: "red",
+    price: 620
+  },
+  {
+    name: "Cherry Charisma",
+    pic: "photos/cherry_charisma.png",
+    color: "red",
+    price: 620
+  },
+  {
+    name: "Cherry Charisma",
+    pic: "photos/cherry_charisma.png",
+    color: "red",
+    price: 620
+  },
+  {
+    name: "Cherry Charisma",
+    pic: "photos/cherry_charisma.png",
+    color: "red",
+    price: 620
+  },
+  {
+    name: "Cherry Charisma",
+    pic: "photos/cherry_charisma.png",
+    color: "red",
+    price: 620
+  },
+  {
+    name: "Cherry Charisma",
+    pic: "photos/cherry_charisma.png",
+    color: "red",
+    price: 620
+  },
+  {
+    name: "Cherry Charisma",
+    pic: "photos/cherry_charisma.png",
+    color: "red",
+    price: 620
+  },
+  {
+    name: "Cherry Charisma",
+    pic: "photos/cherry_charisma.png",
+    color: "red",
+    price: 620
+  },
+  {
+    name: "Cherry Charisma",
+    pic: "photos/cherry_charisma.png",
+    color: "red",
+    price: 620
+  },
+  {
+    name: "Cherry Charisma",
+    pic: "photos/cherry_charisma.png",
+    color: "red",
+    price: 620
+  },
+  {
+    name: "Cherry Charisma",
+    pic: "photos/cherry_charisma.png",
+    color: "red",
+    price: 620
+  },
+  {
+    name: "Cherry Charisma",
+    pic: "photos/cherry_charisma.png",
+    color: "red",
+    price: 620
+  },
+  {
+    name: "Cherry Charisma",
+    pic: "photos/cherry_charisma.png",
+    color: "red",
+    price: 620
+  },
+  {
+    name: "Cherry Charisma",
+    pic: "photos/cherry_charisma.png",
+    color: "red",
+    price: 620
+  },
+  {
+    name: "Cherry Charisma",
+    pic: "photos/cherry_charisma.png",
+    color: "red",
+    price: 620
+  },
+  {
+    name: "Cherry Charisma",
+    pic: "photos/cherry_charisma.png",
+    color: "red",
+    price: 620
+  },
+  {
+    name: "Cherry Charisma",
+    pic: "photos/cherry_charisma.png",
+    color: "red",
+    price: 620
+  },
+  {
+    name: "Cherry Charisma",
+    pic: "photos/cherry_charisma.png",
+    color: "red",
+    price: 620
+  },
+  {
+    name: "Cherry Charisma",
+    pic: "photos/cherry_charisma.png",
+    color: "red",
+    price: 620
+  },
+  {
+    name: "Cherry Charisma",
+    pic: "photos/cherry_charisma.png",
+    color: "red",
+    price: 620
+  },
+  {
+    name: "Cherry Charisma",
+    pic: "photos/cherry_charisma.png",
+    color: "red",
+    price: 620
+  },
+  {
+    name: "Cherry Charisma",
+    pic: "photos/cherry_charisma.png",
+    color: "red",
+    price: 620
+  },
+  {
+    name: "Cherry Charisma",
+    pic: "photos/cherry_charisma.png",
+    color: "red",
+    price: 620
+  },
+  {
+    name: "Cherry Charisma",
+    pic: "photos/cherry_charisma.png",
+    color: "red",
+    price: 620
+  },
+  {
+    name: "Cherry Charisma",
+    pic: "photos/cherry_charisma.png",
+    color: "red",
+    price: 620
+  },
+  {
+    name: "Cherry Charisma",
+    pic: "photos/cherry_charisma.png",
+    color: "red",
+    price: 620
+  },
+  {
+    name: "Cherry Charisma",
+    pic: "photos/cherry_charisma.png",
+    color: "red",
+    price: 620
+  },
+  {
+    name: "Cherry Charisma",
+    pic: "photos/cherry_charisma.png",
+    color: "red",
+    price: 620
+  },
+  {
+    name: "Cherry Charisma",
+    pic: "photos/cherry_charisma.png",
+    color: "red",
+    price: 620
+  },
+  {
+    name: "Cherry Charisma",
+    pic: "photos/cherry_charisma.png",
+    color: "red",
+    price: 620
+  },
+  {
+    name: "Cherry Charisma",
+    pic: "photos/cherry_charisma.png",
+    color: "red",
+    price: 620
+  },
+  {
+    name: "Cherry Charisma",
+    pic: "photos/cherry_charisma.png",
+    color: "red",
+    price: 620
+  },
+  {
+    name: "Cherry Charisma",
+    pic: "photos/cherry_charisma.png",
+    color: "red",
+    price: 620
   }
+  
 
 
 ];
--- fuggvenyek.js
+++ fuggvenyek.js
@@ -6,7 +6,7 @@ export function kartyaLetrehoz(list){
             <div class="kartya" id="${index}">
             <div class="kartya-header"><h2>${elem.name}</h2></div>
             <div class="kartya-body"><img src="${elem.pic}"></div>
-            <div class="kartya-footer"><p id="description">${elem.dsc}</p><p class="price">${elem.price}</p></div>
+            <div class="kartya-footer"><p id="description">${elem.color}</p><p class="price">${elem.price}</p></div>
             </div>
         `
     });
